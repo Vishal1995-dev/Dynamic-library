b07757510df802456a33f90509cfd17de347efc8
Vishal1995-dev <70947761+Vishal1995-dev@users.noreply.github.com>
2020-11-01 20:00:07 +0530

    Add files via upload

--- Main.c
+++ Main.c
@@ -6,16 +6,22 @@
 int main()
 {
 	void *lib=NULL;
+ 	
  	//int add(int no1,int no2)
 	int (*fptr)(int,int);		//function ptr fptr
+	int (*fptr2)(int,int);
+	
 	int val1=10,val2=11,ret=0;
+	
 	printf("Linking library....\n");
+	
 	lib=dlopen("./Library.so",RTLD_LAZY);
 	if(lib==NULL)
 	{
 		printf("Unable to link library\n");
 		return-1;
 	}
+	
 	printf("Library linked successfully\n");
 	
 	printf("Fetching address of add\n");
@@ -26,11 +32,31 @@ int main()
 		printf("Unable to get address\n");
 		return-1;
 	}
+	
 	printf("Address fetched successfully\n");
+	
 	ret=fptr(val1,val2);
+	
 	printf("Addition is : %d\n",ret);
 	
+	printf("Fetching address of mul\n");
+	
+	fptr2=dlsym(lib,"mul");
+	if(fptr2==NULL)
+	{
+		printf("Unable to get address\n");
+		return-1;
+	}
+	
+	printf("Address fetched successfully\n");
+	
+	ret=fptr2(val1,val2);
+	
+	printf("Multiplication is : %d\n",ret);
+	
 	dlclose(lib);
+	
 	printf("Dynamic library unlinked\n");
+	
 	return 0;
 }
